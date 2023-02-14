478bd06d407518c1615f916169fa3d6487c335d9
Jaspreet1616kaur <jk1195383@gmail.com>
2023-02-14 19:12:19 +0100

    new commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//
+
+let myName = "jaspreetkaur";
+console.log("my name is", myName);
+
+//age
+let myAge = 34;
+console.log("my age is ", myAge);
+
+//julia age
+let juliaAge = 32;
+
+let ageDiff = Age – 32;
+console.log("juliAge", ageDiff);
+
+
