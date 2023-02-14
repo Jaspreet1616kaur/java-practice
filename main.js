5624d450eb0a66f1287187be3f990c2ef026499d
Jaspreet1616kaur <jk1195383@gmail.com>
2023-02-14 19:15:05 +0100

    new

--- main.js
+++ main.js
@@ -10,7 +10,24 @@ console.log("my age is ", myAge);
 //julia age
 let juliaAge = 32;
 
-let ageDiff = Age – 32;
-console.log("juliAge", ageDiff);
+let ageDiff = 58 - 32;
+console.log("julia age", ageDiff);
 
+// here used some conditinal statement
+let jasAge = 5;
+if (jasAge < 21) {
+  console.log("you are not older than  julia");
+} else {
+  console.log("you are older then julia");
+}
 
+//compare age withjuiaAge
+juliaAge = 13;
+myAge = 73;
+if (juliaAge < myAge) {
+  console.log("julia is younger then me ");
+} else if (juliaAge == myAge) {
+  console.log("You have the same age as Julia");
+} else {
+  console.log("julia is older then me");
+}
